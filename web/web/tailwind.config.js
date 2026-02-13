c304a0793f8323288ac6d2d28d2fc0c23a55e9c8
Lubos Kukla <66630194+LubosKukla@users.noreply.github.com>
2026-02-13 10:57:09 +0100

    tailwind a zakladne nastavenia

---
+++ web/web/tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#EF554A",
+        heading: "#333333",
+        light: "#F6F6F6",
+        dark: "#272727",
+        black: "#000000",
+        white: "#FFFFFF",
+      },
+      fontFamily: {
+        sans: ['"DM Sans"', ...defaultTheme.fontFamily.sans],
+        display: ['"Clash Display"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
