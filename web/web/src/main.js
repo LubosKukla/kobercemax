c47588782aae72b634924bfdf276491cdd438aac
Lubos Kukla <66630194+LubosKukla@users.noreply.github.com>
2026-02-13 11:04:14 +0100

    pridanie pluginu font awesome

--- web/web/src/main.js
+++ web/web/src/main.js
@@ -1,8 +1,19 @@
 import { createApp } from "vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import App from "./App.vue";
 import "./assets/tailwind.css";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+library.add(fas, far, fab);
+
+createApp(App)
+  .component("FontAwesomeIcon", FontAwesomeIcon)
+  .use(store)
+  .use(router)
+  .mount("#app");
